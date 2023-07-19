14242df455062ac0eaaf00147cf0014027674ac5
Enzo Sandré <atelier@enzosandre.fr>
2023-07-19 10:38:45 +0200

    Division en 2 classes partielles

---
+++ AfficheurLCD.Console/Chiffres.cs
@@ -0,0 +1,54 @@
+﻿namespace AfficheurLCD.Console;
+
+internal partial class Nombre
+{
+    private static readonly Nombre Un = new(
+        "   ",
+        "  |",
+        "  |");
+
+    private static readonly Nombre Deux = new(
+        " _ ",
+        " _|",
+        "|_ ");
+
+    private static readonly Nombre Trois = new(
+        " _ ",
+        " _|",
+        " _|");
+
+    private static readonly Nombre Quatre = new(
+        "   ",
+        "|_|",
+        "  |");
+
+    private static readonly Nombre Cinq = new(
+        " _ ",
+        "|_ ",
+        " _|");
+
+    private static readonly Nombre Six = new(
+        " _ ",
+        "|_ ",
+        "|_|");
+
+    private static readonly Nombre Sept = new(
+        " _ ",
+        "  |",
+        "  |");
+
+    private static readonly Nombre Huit = new(
+        " _ ",
+        "|_|",
+        "|_|");
+
+    private static readonly Nombre Neuf = new(
+        " _ ",
+        "|_|",
+        " _|");
+
+    private static readonly Nombre Zéro = new(
+        " _ ",
+        "| |",
+        "|_|");
+}
--- AfficheurLCD.Console/Nombre.cs
+++ AfficheurLCD.Console/Nombre.cs
@@ -1,57 +1,7 @@
 ﻿namespace AfficheurLCD.Console;
 
-internal class Nombre
+internal partial class Nombre
 {
-    private static readonly Nombre Un = new (
-        "   ", 
-        "  |", 
-        "  |");
-
-    private static readonly Nombre Deux = new (
-        " _ ", 
-        " _|", 
-        "|_ ");
-
-    private static readonly Nombre Trois = new(
-        " _ ",
-        " _|",
-        " _|");
-
-    private static readonly Nombre Quatre = new(
-        "   ",
-        "|_|",
-        "  |");
-
-    private static readonly Nombre Cinq = new(
-        " _ ",
-        "|_ ",
-        " _|");
-
-    private static readonly Nombre Six = new(
-        " _ ",
-        "|_ ",
-        "|_|");
-
-    private static readonly Nombre Sept = new(
-        " _ ",
-        "  |",
-        "  |");
-
-    private static readonly Nombre Huit = new(
-        " _ ",
-        "|_|",
-        "|_|");
-
-    private static readonly Nombre Neuf = new(
-        " _ ",
-        "|_|",
-        " _|");
-
-    private static readonly Nombre Zéro = new(
-        " _ ",
-        "| |",
-        "|_|");
-
     private readonly string _ligne1;
     private readonly string _ligne2;
     private readonly string _ligne3;
